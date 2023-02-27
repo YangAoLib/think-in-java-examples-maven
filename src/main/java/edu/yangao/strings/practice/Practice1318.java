436c7ae4bb9504a12a02f41ca11c8b3ca931475e
YangAo <ya1398789131@163.com>
2023-02-27 09:10:33 +0800

    1318、1319

---
+++ src/main/java/edu/yangao/strings/practice/Practice1318.java
@@ -0,0 +1,35 @@
+package edu.yangao.strings.practice;
+
+import edu.yangao.net.mindview.util.TextFile;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author YangAo
+ */
+public class Practice1318 {
+
+    /**
+     * 注释的匹配正则
+     */
+    private static final Pattern EXEGESIS = Pattern.compile(".*?class\\s+(.*?)\\s+\\{", Pattern.MULTILINE);
+
+    /**
+     * 匹配器
+     */
+    private static final Matcher MATCHER = EXEGESIS.matcher("");
+
+    /**
+     * 文件路径
+     */
+    public static final String FILE_PATH = "src/main/java/edu/yangao/strings/practice/";
+    public static void main(String[] args) {
+        for (String filePath : Practice1317.readFiles(FILE_PATH)) {
+            MATCHER.reset(TextFile.read(filePath));
+            while (MATCHER.find()) {
+                System.out.println(MATCHER.group(1));
+            }
+        }
+    }
+}
